b128e73d5a126d3afde0eb4a0553f95294b17ed9
Sabarinathan <sabarinathan18p@gmail.com>
2026-04-09 10:52:30 +0530

    Change createdBy from 'MoitheGreat' to 'sabari'

--- src/test/java/ninjaHRM/AddProject.java
+++ src/test/java/ninjaHRM/AddProject.java
@@ -10,7 +10,7 @@ public class AddProject {
 		.contentType("application/xml")
         .body("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
         		+ "<Project>\r\n"
-        		+ "	<createdBy>MoitheGreat</createdBy>\r\n"
+        		+ "	<createdBy>sabari</createdBy>\r\n"
         		+ "	<projectName>test321</projectName>\r\n"
         		+ "	<status>Completed</status>\r\n"
         		+ "	<teamSize>0</teamSize>\r\n"
