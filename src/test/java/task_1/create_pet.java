d109fd4941efb88f33007534d6818efdf662f11b
Sabarinathan <sabarinathan18p@gmail.com>
2026-04-09 10:48:10 +0530

    Rename class from botter to create_pet

--- src/test/java/task_1/create_pet.java
+++ src/test/java/task_1/create_pet.java
@@ -4,13 +4,13 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 
-public class botter {
+public class create_pet {
 	@Test
 	
 	public static void lol(){
 		
 		
-		//File json = new File("C:\\Users\\HARWIN S\\eclipse-workspace\\lotOfLove\\src\\test\\resources\\testing.json");
+		//File json = new File("C:\\Users\\sabari\\eclipse-workspace\\create\\src\\test\\resources\\testing.json");
 		RestAssured.given()
 		.header("Content-Type","application/json")
 		.body(new object())
