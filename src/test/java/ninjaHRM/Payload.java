b74be9f95f8d89d2ee8317c4a203521fc0282637
Sabarinathan <sabarinathan18p@gmail.com>
2026-04-09 10:53:53 +0530

    Update createdBy field in project XML payload

--- src/test/java/ninjaHRM/Payload.java
+++ src/test/java/ninjaHRM/Payload.java
@@ -4,7 +4,7 @@ public class Payload {
 	public static String CreateAProject() {
 		return "<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\\r\\n\"\r\n"
 				+ "        		+ \"<Project>\\r\\n\"\r\n"
-				+ "        		+ \"	<createdBy>MoitheGreat</createdBy>\\r\\n\"\r\n"
+				+ "        		+ \"	<createdBy>sabari</createdBy>\\r\\n\"\r\n"
 				+ "        		+ \"	<projectName>test321</projectName>\\r\\n\"\r\n"
 				+ "        		+ \"	<status>Completed</status>\\r\\n\"\r\n"
 				+ "        		+ \"	<teamSize>0</teamSize>\\r\\n\"\r\n"
